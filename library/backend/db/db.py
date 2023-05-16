a89a968c2acb915c8631c2a15d408315dbf0bf58
ParkieV <st-psk@mail.ru>
2023-05-16 23:58:40 +0300

    Added migrations and created ReactJS app

--- library/backend/db/db.py
+++ library/backend/db/db.py
@@ -4,9 +4,6 @@ from backend.db.models import Base
 from backend.db.settings import DBSettings
 
 settings = DBSettings()
-
-
-def init_db():
-    
-    engine = create_engine(f"postgresql+psycopg2://{settings.username}:{settings.password}@{settings.host}:{settings.port}/{settings.database}")
-    Base.metadata.create_all(bind=engine)
+DATABASE_URL = f"postgresql+psycopg2://{settings.username}:{settings.password}@{settings.host}:{settings.port}/{settings.database}"
+engine = create_engine(DATABASE_URL)
+Base.metadata.create_all(bind=engine)
